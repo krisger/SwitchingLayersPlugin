db1f26799b03c6d20f9c92438689a8f055afbad8
unknown <kristijonasgerdvilis@gmail.com>
2018-06-05 23:42:40 +0300

    Switching Layers Plugin

---
+++ readme.md
@@ -0,0 +1,33 @@
+<h2>Instalation</h2>
+<p>Get from "src" folder plugin "switching-layers.js" add it to you header under jQuery library included</p>
+<p>This plugin requires jQuery to be included </p>
+<p>In example used bootstrap, but you can build layout without it</p>
+<p>Have in mind what each data-switch="switch-parent" children top div elements should be absolute and top on each other to show as in example</p>
+<p>In your css, you will need to hide with css (display: none) data-switch="switch-parent" children top div elements and show (display: block) when active class added</p>
+<p>
+	Call plugin with:
+</p>
+<p>
+<pre lang="no-highlight">
+<code>
+$(function(){
+	$().switchinglayers({});
+});
+
+//or if you want change options
+
+$(function(){
+	$().switchinglayers({
+		mainLayer: $("[data-switch='main-layer']"), //you can change el selectors
+        parentSwitch: $("[data-switch='switch-parent']"), //you can change el selectors
+        switchingTime: 3, //pass seconds, it will slow down or increase speed of switching layers
+		activeClassName: "active" //you can change active class name
+	});
+});
+
+
+</code>
+</pre>
+</p>
+<h2><a id="user-content-license" class="anchor" aria-hidden="true" href="#license"></a>License</h2>
+<p>This project is licensed under the MIT License - for this read License.md file</p>
---
+++ src/switching-layers.js
@@ -0,0 +1,65 @@
+$.fn.switchinglayers = function( options ) 
+{
+    var mainLayer, switchArea, layerCounter = [];
+    var sett = $.extend({
+        container: $("[data-switch='container']"),
+        mainLayer: $("[data-switch='main-layer']"),
+        parentSwitch: $("[data-switch='switch-parent']"),
+		activeClassName: "active",
+        switchingTime: 3 //seconds
+    });
+
+    changeLayer(sett, true);
+    setInterval
+    (
+        function()
+        {
+
+            if(sett.container.length > 0)
+            {
+                changeLayer(sett, false);
+            }
+        }, 
+        (sett.switchingTime * 1000)
+    )
+    
+    function changeLayer(sett, firstLoad)
+    {
+        var sCount = 0;
+        for(var j = 0; j < layerCounter.length; j++)
+        {  
+            layerCounter[j] += 1; 
+        }
+
+        $(sett.container).each
+        (function()
+        {
+           
+            mainLayer = $(this).find(sett.mainLayer);
+
+            $(mainLayer).each(function(){  
+                switchArea = $(this).find(sett.parentSwitch);	  
+
+                
+                switchArea.each(function()
+                {
+                    sCount++; 
+
+                    if(firstLoad){
+                        layerCounter.push(0);
+                    } 
+
+                    if($(this).children("div").eq(layerCounter[sCount - 1]).length == 0)
+                    {
+                        layerCounter[sCount - 1] = 0;
+                    }
+
+                    $(this).children("div").removeClass(sett.activeClassName)
+                    $(this).children("div").eq(layerCounter[sCount-1]).addClass(sett.activeClassName);   
+                });
+            });
+        }
+        );	
+    }
+    
+}
